f70f1f747b9c11781c7a1edfa811e5dfc0bca4f5
Theo Rocha <theo.mrocha@tst.jus.br>
2023-10-25 17:03:39 -0300

    first API consumation & nodemon add

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
 const express = require("express");
 const axios = require("axios");
-const config = require("/config");
+const https = require("https");
+const config = require("./config");
 
 const apiKey = config.apiKey;
 
 const app = express();
 const port = 8081;
 
-app.get("/api");
+app.get("/api/customers", async (req, res) => {
+  const options = {
+    method: "GET",
+    url: "https://sandbox.asaas.com/api/v3/customers",
+    headers: {
+      accept: "application/json",
+      access_token:
+        "$aact_YTU5YTE0M2M2N2I4MTliNzk0YTI5N2U5MzdjNWZmNDQ6OjAwMDAwMDAwMDAwMDAwNjgwNzU6OiRhYWNoX2IyNWNkZjJhLTIxYTktNGU2NS05MjU4LWIwOTM1MGYzZjI0Ng==",
+    },
+    httpsAgent: new https.Agent({
+      rejectUnauthorized: false,
+    }),
+  };
 
-app.get("https://sandbox.asaas.com/api/v3/customers");
+  axios
+    .request(options)
+    .then(function (response) {
+      const data = response.data;
+      res.send(data);
+    })
+    .catch(function (error) {
+      res
+        .status(500)
+        .json({ error: "Erro ao listar clientes da API externa." });
+    });
+});
 
 app.listen(port, () => {
   console.log(`Servidor Express rodando na porta ${port}`);
