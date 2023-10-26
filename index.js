ba41bb79707b848fa1ce79cd13c25660387a363d
Theo Rocha <theomarquesrx@gmail.com>
2023-10-26 01:06:47 -0300

    payments working

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const axios = require("axios");
 const https = require("https");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const config = require("./config");
 
 const apiKey = config.apiKey;
@@ -8,14 +10,16 @@ const apiKey = config.apiKey;
 const app = express();
 const port = 8081;
 
+app.use(cors());
+app.use(bodyParser.json());
+
 app.get("/api/customers", async (req, res) => {
   const options = {
     method: "GET",
     url: "https://sandbox.asaas.com/api/v3/customers",
     headers: {
       accept: "application/json",
-      access_token:
-        "$aact_YTU5YTE0M2M2N2I4MTliNzk0YTI5N2U5MzdjNWZmNDQ6OjAwMDAwMDAwMDAwMDAwNjgwNzU6OiRhYWNoX2IyNWNkZjJhLTIxYTktNGU2NS05MjU4LWIwOTM1MGYzZjI0Ng==",
+      access_token: apiKey,
     },
     httpsAgent: new https.Agent({
       rejectUnauthorized: false,
@@ -34,6 +38,89 @@ app.get("/api/customers", async (req, res) => {
         .json({ error: "Erro ao listar clientes da API externa." });
     });
 });
+
+app.get("/api/customers/:id", async (req, res) => {
+  const customerId = req.params.id;
+
+  const options = {
+    method: "GET",
+    url: `https://sandbox.asaas.com/api/v3/customers/${customerId}`,
+    headers: {
+      accept: "application/json",
+      access_token: apiKey,
+    },
+    httpsAgent: new https.Agent({
+      rejectUnauthorized: false,
+    }),
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      const data = response.data;
+      res.send(data);
+    })
+    .catch(function (error) {
+      res.status(500).json({
+        error: "Erro ao obter informações do cliente da API externa.",
+      });
+    });
+});
+app.post("/api/customers", async (req, res) => {
+  const postData = req.body;
+  console.log(postData);
+
+  const options = {
+    method: "POST",
+    url: "https://sandbox.asaas.com/api/v3/customers",
+    headers: {
+      accept: "application/json",
+      "content-type": "application/json",
+      access_token: apiKey,
+    },
+    data: postData,
+    httpsAgent: new https.Agent({
+      rejectUnauthorized: false,
+    }),
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      res.sendStatus(201);
+    })
+    .catch(function (error) {
+      console.log(error);
+      res.status(500).json({ error: "Erro ao criar cliente na API externa." });
+    });
+});
+
+app.get("/api/payments", async (req, res) => {
+  const options = {
+    method: "GET",
+    url: "https://sandbox.asaas.com/api/v3/payments",
+    headers: {
+      accept: "application/json",
+      access_token: apiKey,
+    },
+    httpsAgent: new https.Agent({
+      rejectUnauthorized: false,
+    }),
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      const data = response.data;
+      res.send(data);
+    })
+    .catch(function (error) {
+      res
+        .status(500)
+        .json({ error: "Erro ao listar cobranças da API externa." });
+    });
+});
+
 
 app.listen(port, () => {
   console.log(`Servidor Express rodando na porta ${port}`);
