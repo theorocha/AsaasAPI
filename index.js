21c520ebc14115e0295e5b35d520baa38d8146ca
Theo Rocha <theo.mrocha@tst.jus.br>
2023-10-27 18:17:36 -0300

    final commit

--- index.js
+++ index.js
@@ -33,9 +33,7 @@ app.get("/api/customers", async (req, res) => {
       res.send(data);
     })
     .catch(function (error) {
-      res
-        .status(500)
-        .json({ error: "Erro ao listar clientes da API externa." });
+      res.status(500).json({ error: "Erro ao listar clientes." });
     });
 });
 
@@ -62,7 +60,35 @@ app.get("/api/customers/:id", async (req, res) => {
     })
     .catch(function (error) {
       res.status(500).json({
-        error: "Erro ao obter informações do cliente da API externa.",
+        error: "Erro ao obter informações do cliente.",
+      });
+    });
+});
+
+app.delete("/api/customers/:id", async (req, res) => {
+  const customerId = req.params.id;
+
+  const options = {
+    method: "GET",
+    url: `https://sandbox.asaas.com/api/v3/customers/${customerId}`,
+    headers: {
+      accept: "application/json",
+      access_token: apiKey,
+    },
+    httpsAgent: new https.Agent({
+      rejectUnauthorized: false,
+    }),
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      const data = response.data;
+      res.status(204).send(data);
+    })
+    .catch(function (error) {
+      res.status(500).json({
+        error: "Erro ao excluir cliente.",
       });
     });
 });
@@ -91,7 +117,7 @@ app.post("/api/customers", async (req, res) => {
     })
     .catch(function (error) {
       console.log(error);
-      res.status(500).json({ error: "Erro ao criar cliente na API externa." });
+      res.status(500).json({ error: "Erro ao criar cliente." });
     });
 });
 
@@ -117,7 +143,35 @@ app.post("/api/payments", async (req, res) => {
     })
     .catch(function (error) {
       console.log(error);
-      res.status(500).json({ error: "Erro ao criar cobrança na API externa." });
+      res.status(500).json({ error: "Erro ao criar cobrança." });
+    });
+});
+
+app.post("/api/pix-payment", async (req, res) => {
+  const postData = req.body;
+  const options = {
+    method: "POST",
+    url: "https://sandbox.asaas.com/api/v3/pix/qrCodes/pay",
+    headers: {
+      accept: "application/json",
+      "content-type": "application/json",
+      access_token: apiKey,
+    },
+    data: postData,
+    httpsAgent: new https.Agent({
+      rejectUnauthorized: false,
+    }),
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      res.send(response.data);
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      res.status(500).json({ error: "Erro ao pagar cobrança." });
+      console.error(error);
     });
 });
 
@@ -141,9 +195,7 @@ app.get("/api/payments", async (req, res) => {
       res.send(data);
     })
     .catch(function (error) {
-      res
-        .status(500)
-        .json({ error: "Erro ao listar cobranças da API externa." });
+      res.status(500).json({ error: "Erro ao listar cobranças." });
     });
 });
 
